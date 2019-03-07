23559312741868d703c05b82e466e8e912714d47
Guy Spronck <guy.spronck@qnh.nl>
2019-03-07 11:28:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,68 @@
-var express = require('express');
-var request = require('request');
-var cors = require('cors');
-var FeedParser = require('feedparser');
+var express = require("express");
+var request = require("request");
+var cors = require("cors");
+var FeedParser = require("feedparser");
 
 var app = express();
 app.use(cors());
 module.exports.app = app;
-app.set('port', (process.env.PORT || 5000));
-
-var testing = [];
-/*FeedParser Test Code*/
-app.get('/', function(req1,res1){
-var url = req1.query.url;
-  if(!url.match("^(http|https|ftp)://.*$")){
-    var protocol = 'http://';
-    if(url.indexOf('//') == 0)
-      protocol = 'http:';
-  url = protocol+url;
+app.set("port", process.env.PORT || 5000);
+app.get("/", function(incoming_request, response) {
+  var url = incoming_request.query.url;
+  if (url === undefined){
+    response.send(`Usage: ${incoming_request.headers.host}/?url=feedURL`);
+    return;
   }
-var req = request(url);
-var feedparser = new FeedParser();
-
-var collection = [];
-
-req.on('error', function (error) {
-  console.error(error);
-});
-
-req.on('response', function (res) {
-  var stream = this; // `this` is `req`, which is a stream
-
-  if (res.statusCode !== 200) {
-    this.emit('error', new Error('Bad status code'));
+  if (!url.match("^(http|https|ftp)://.*$")) {
+    var protocol = "http://";
+    if (url.indexOf("//") == 0) protocol = "http:";
+    url = protocol + url;
   }
-  else {
-    stream.pipe(feedparser);
-  }
-});
-feedparser.on('error', function (error) {
-  console.error(error);
+
+
+  var req = request(url);
+  var feedparser = new FeedParser();
+
+  var collection = [];
+
+  req.on("error", function(error) {
+    console.error(error);
+    response.send('Could not get the requested URL.');
+  });
+
+  req.on("response", function(res) {
+    var stream = this; // `this` is `req`, which is a stream
+
+    if (res.statusCode !== 200) {
+      this.emit("error", new Error("Bad status code"));
+    } else {
+      stream.pipe(feedparser);
+    }
+  });
+  feedparser.on("error", function(error) {
+    console.error(error);
+  });
+
+  feedparser.on("readable", function() {
+    var stream = this; // `this` is `feedparser`, which is a stream
+    var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+    var item;
+    //Add articles to collection
+    while ((item = stream.read())) {
+      collection.push(item); //array of objects
+    }
+  });
+
+  feedparser.on("end", function() {
+    var stream = this;
+    var meta = this.meta;
+    var finalResponse = {};
+    finalResponse.entries = collection;
+    finalResponse.meta = meta;
+    response.send(finalResponse);
+  });
 });
 
-feedparser.on('readable', function () {
-  var stream = this; // `this` is `feedparser`, which is a stream
-  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-  var item;
-//Add articles to collection
-  while (item = stream.read()) {
-    collection.push(item);    //array of objects
-  }
-});
-
-feedparser.on('end', function () {
-  var stream = this;
-  var meta = this.meta;
-  var finalResponse = {};
-  finalResponse.entries = collection;
-  finalResponse.meta = meta;
-  res1.send(finalResponse);
-});
-});
-/*END FeedParser Test Code*/
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+  console.log("Feed to Json is running on port", app.get("port"));
 });
